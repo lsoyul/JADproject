6219abfa8afff4c6a5e8eeec86d46b2bc04211f6
KSG <sgykim15@naver.com>
2017-04-10 14:47:34 +0900

    1. Add new fonts ("소야뜰9" for in game dialogs / "power_pixel-7" for big pixelic title or somethings) 2. Add ManagerController.cs (Create or Destruct managers) 3. Add GameSceneManager (A manager for scene control) 4. Add Titlescene (scene_title)

---
+++ JAR/Assets/Scripts/Manager/GameSceneManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum EGameScene
+{
+    NONE,
+    TITLE,
+    VILLAGE,
+    BATTLESTAGE,
+    TEST
+}
+
+public class GameSceneManager : ManagerTemplate<GameSceneManager> {
+
+    private EGameScene currentScene = EGameScene.NONE;
+    public EGameScene CurrentScene { get { return currentScene; } }
+
+    public void GotoScene(EGameScene scene)
+    {
+        switch(scene)
+        {
+            case EGameScene.NONE:
+                break;
+            case EGameScene.TITLE:
+                break;
+            case EGameScene.VILLAGE:
+                break;
+            case EGameScene.BATTLESTAGE:
+                StartCoroutine("AsynchronousLoad", "scene_gaming");
+                break;
+            case EGameScene.TEST:
+                break;
+        }
+    }
+    
+    IEnumerator AsynchronousLoad(string scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while(!ao.isDone)
+        {
+            // [0, 0.9] > [0, 1]
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            // Loading completed
+            if (ao.progress == 0.9f)
+            {
+                //Debug.Log("Press a key to start");
+                //if (Input.anyKeyDown)
+                    ao.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ JAR/Assets/Scripts/Manager/ManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerController : MonoBehaviour {
+
+    void Awake()
+    {
+        this.gameObject.name = "[System]ManagerController";
+        DontDestroyOnLoad(this.gameObject);
+
+        InitManager();
+    }
+
+    public void StartGame()
+    {
+        GameSceneManager.Instance.GotoScene(EGameScene.BATTLESTAGE);
+    }
+	
+
+    private void InitManager()
+    {
+        GameSceneManager.Create();
+    }
+}
